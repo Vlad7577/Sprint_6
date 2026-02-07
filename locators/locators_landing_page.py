fc1df535362c2393834edf4ad7a7f45611905bea
Vlad7577 <vlad-gezik@mail.ru>
2026-02-07 19:10:48 +0500

    Update locators_landing_page.py

--- locators/locators_landing_page.py
+++ locators/locators_landing_page.py
@@ -1,16 +1,21 @@
 from selenium.webdriver.common.by import By
 
-class TestLocator_landing_questions:
 
-    Button_questions_about_important = By.ID, "accordion__heading-{}"
-    QUESTION_8 = By.ID, 'accordion__heading-7'
+class test_locator_landing_questions:
 
-class TestLocator_landing_answer:
+    BUTTON_QUESTION = (By.ID, "accordion__heading-{}")
+    QUESTION_8 = (By.ID, "accordion__heading-7")
 
-    Text_answer_question_about_important = By.ID, "accordion__panel-{}"
 
-class Testlocators:
-    Button_make_order_header = By.XPATH, '//div[@class="Header_Nav__AGCXC"]/button[text()="Заказать"]'
-    Button_make_order_landing = By.XPATH, '//div[@class="Home_FinishButton__1_cWm"]/button[text()="Заказать"]'
+class test_locator_landing_answer:
+
+    TEXT_ANSWER = (By.ID, "accordion__panel-{}")
+
+
+class test_locators:
+
+    BUTTON_MAKE_ORDER_HEADER = (By.XPATH, '//button[text()="Заказать"]')
+    BUTTON_MAKE_ORDER_LANDING = (By.XPATH, '//button[text()="Заказать"]')
+
     LOGO_SCOOTER = (By.CSS_SELECTOR, ".Header_LogoScooter__3lsAR img")
     YA_LOGO = (By.CSS_SELECTOR, "img[src='/assets/ya.svg']")
