97cddc0e1758da3fa59666a871f95b6315073807
Vlad7577 <vlad-gezik@mail.ru>
2026-02-07 19:40:47 +0500

    Update landing_page.py

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -1,30 +1,40 @@
-from pages.base_page import BasePage
-from locators.locators_landing_page import TestLocator_landing_questions
-from locators.locators_landing_page import TestLocator_landing_answer
-from locators.locators_landing_page import Testlocators
 import allure
+from pages.base_page import BasePage
+
+from locators.locators_landing_page import (
+    test_locator_landing_questions,
+    test_locator_landing_answer,
+    test_locators
+)
+
 
 class LandingPage(BasePage):
 
-    def __init__(self, driver):
-        super().__init__(driver)
+    def _init_(self, driver):
+        super()._init_(driver)
 
-    @allure.step('Прокрутить страницу до блока вопросов')
+    @allure.step("Прокрутить страницу до блока вопросов")
     def scroll_to_questions(self):
-        self.scroll_to_element(TestLocator_landing_questions.QUESTION_8)
+        self.scroll_to_element(test_locator_landing_questions.QUESTION_8)
 
-    @allure.step('Кликнуть по вопросу')
+    @allure.step("Кликнуть по вопросу")
     def click_on_question(self, question_num):
-        formatted_questions_locator = self.format_locator(TestLocator_landing_questions.Button_questions_about_important, question_num)
-        self.click_on_element(formatted_questions_locator)
+        formatted_locator = self.format_locator(
+            test_locator_landing_questions.BUTTON_QUESTION,
+            question_num
+        )
+        self.click_on_element(formatted_locator)
 
-    @allure.step('Получить текст ответа')
+    @allure.step("Получить текст ответа")
     def get_answer_text(self, question_num):
-        formatted_text_answer_locator = self.format_locator(TestLocator_landing_answer.Text_answer_question_about_important, question_num)
-        return self.get_text_from_element(formatted_text_answer_locator)
+        formatted_locator = self.format_locator(
+            test_locator_landing_answer.TEXT_ANSWER,
+            question_num
+        )
+        return self.get_text_from_element(formatted_locator)
 
-    @allure.step('Клик по кнопке Заказать вверху лэндинга')
-    def click_on_order_button(self):  # Измените имя метода
-        self.click_on_element(Testlocators.Button_make_order_header)
+    @allure.step("Клик по кнопке Заказать вверху страницы")
+    def click_on_order_button(self):
+        self.click_on_element(test_locators.BUTTON_MAKE_ORDER_HEADER)
 
 
