df32eecb2cb301fab2dc0386c57c9ce5d5510c73
Vlad7577 <vlad-gezik@mail.ru>
2026-02-07 19:39:01 +0500

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,41 +1,41 @@
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class BasePage:
-    BASE_URL = 'https://qa-scooter.praktikum-services.ru/' # URL здесь
 
-    def __init__(self, driver):
+    def _init_(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 20)
+        self.wait = WebDriverWait(driver, 10)
 
-    def open_url(self):
-        self.driver.get(self.BASE_URL)
+    def open_url(self, url):
+        self.driver.get(url)
 
-    def scroll_to_element(self, locator):
-        element = self.driver.find_element(*locator)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
-
-    def scroll_into_view(self, locator):
-        element = self.wait.until(expected_conditions.presence_of_element_located(locator))
-        self.driver.execute_script('arguments[0].scrollIntoView();', element)
-
-    def find_element_with_wait(self, locator):
-        return self.wait.until(expected_conditions.visibility_of_element_located(locator))
+    def find_element(self, locator):
+        return self.wait.until(
+            EC.presence_of_element_located(locator)
+        )
 
     def click_on_element(self, locator):
-        element = self.find_element_with_wait(locator)
+        element = self.wait.until(
+            EC.element_to_be_clickable(locator)
+        )
         element.click()
 
     def get_text_from_element(self, locator):
-        element = self.find_element_with_wait(locator)
+        element = self.find_element(locator)
         return element.text
 
-    def format_locator(self, locator, num):
-        method, locator = locator
-        locator = locator.format(num)
-        return (method, locator)
+    def scroll_to_element(self, locator):
+        element = self.find_element(locator)
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView();",
+            element
+        )
+
+    def format_locator(self, locator, value):
+        by, path = locator
+        return by, path.format(value)
 
     def get_current_url(self):
         return self.driver.current_url
