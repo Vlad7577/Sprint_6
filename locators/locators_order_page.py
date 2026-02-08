3a51eed3c8dbfdd62fdb960d18207dc0b7df89e7
Vlad7577 <vlad-gezik@mail.ru>
2026-02-08 22:14:45 +0500

    Update locators_order_page.py

--- locators/locators_order_page.py
+++ locators/locators_order_page.py
@@ -1,16 +1,16 @@
 from selenium.webdriver.common.by import By
 
 
-class Order_Page_Locators:
+class OrderPageLocators:
 
-    INPUT_NAME = (By.CSS_SELECTOR, "input[placeholder='* Имя']")
-    INPUT_SURNAME = (By.CSS_SELECTOR, "input[placeholder='* Фамилия']")
-    INPUT_ADDRESS = (By.CSS_SELECTOR, "input[placeholder='* Адрес: куда привезти заказ']")
-    INPUT_PHONE = (By.CSS_SELECTOR, "input[placeholder='* Телефон: на него позвонит курьер']")
+    INPUT_NAME = (By.CSS_SELECTOR, "input[placeholder*='Имя']")
+    INPUT_SURNAME = (By.CSS_SELECTOR, "input[placeholder*='Фамилия']")
+    INPUT_ADDRESS = (By.CSS_SELECTOR, "input[placeholder*='Адрес']")
+    INPUT_PHONE = (By.CSS_SELECTOR, "input[placeholder*='Телефон']")
 
     BUTTON_NEXT = (By.XPATH, "//button[text()='Далее']")
 
-    INPUT_DATE = (By.CSS_SELECTOR, "input[placeholder='* Когда привезти самокат?']")
+    INPUT_DATE = (By.CSS_SELECTOR, "input[placeholder*='Когда привезти самокат?']")
     DATE_OPTION = (By.XPATH, "//div[text()='{}']")
 
     DROPDOWN_RENTAL_PERIOD = (By.CLASS_NAME, "Dropdown-root")
@@ -20,5 +20,5 @@ class Order_Page_Locators:
 
     CREATE_ORDER_BUTTON = (By.XPATH, "//button[text()='Заказать']")
     CONFIRM_YES_BUTTON = (By.XPATH, "//button[text()='Да']")
-    
+
     ORDER_SUCCESS = (By.CLASS_NAME, "Order_ModalHeader")
