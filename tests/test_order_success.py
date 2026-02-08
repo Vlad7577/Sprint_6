47653e729e23a51f48ca88017b945fc34ef752f1
Vlad7577 <vlad-gezik@mail.ru>
2026-02-08 23:21:51 +0500

    Update test_order_success.py

--- tests/test_order_success.py
+++ tests/test_order_success.py
@@ -1,32 +1,34 @@
-from pages.order_page import OrderPage
 import allure
 import pytest
+from pages.order_page import OrderPage
 
 
-@allure.title("Оформление заказа самоката через кнопки Заказать")
-@pytest.mark.parametrize('button', ['header_button', 'page_button'])
-def test_order_upp_success(driver, button):
+class TestOrderPage:
 
-    order_page = OrderPage(driver)
-    order_page.open_url()
+    @allure.title("Оформление заказа самоката через кнопки Заказать")
+    @pytest.mark.parametrize("button", ["header_button", "page_button"])
+    def test_order_success(self, driver, button):
 
-    with allure.step("Проверяем возможность сделать заказ самоката"):
-        order_page.click_on_element_make_order_header(button=button)
-        order_page.name_field_find_element_with_wait()
-        order_page.fill_name_field()
-        order_page.fill_surname_field()
-        order_page.fill_adress_field()
-        order_page.select_metro()
-        order_page.fill_phone_field()
-        order_page.click_on_element_next()
-        order_page.pick_date()
-        order_page.pick_rental_period()
-        order_page.click_on_element_color_black()
-        order_page.fill_comment_field()
-        order_page.click_on_element_create_order()
-        order_page.click_on_element_confirm_order_yes()
+        order_page = OrderPage(driver)
+        order_page.open_url()
 
-    with allure.step("Проверяем успешное оформление заказа"):
-        assert order_page.is_order_success(), "Заказ не был оформлен"
+        with allure.step("Проверяем возможность сделать заказ самоката"):
+            order_page.click_on_element_make_order_header(button=button)
+            order_page.name_field_find_element_with_wait()
+            order_page.fill_name_field()
+            order_page.fill_surname_field()
+            order_page.fill_adress_field()
+            order_page.select_metro()
+            order_page.fill_phone_field()
+            order_page.click_on_element_next()
+            order_page.pick_date()
+            order_page.pick_rental_period()
+            order_page.click_on_element_color_black()
+            order_page.fill_comment_field()
+            order_page.click_on_element_create_order()
+            order_page.click_on_element_confirm_order_yes()
+
+        with allure.step("Проверяем успешное оформление заказа"):
+            assert order_page.is_order_success(), "Заказ не был оформлен"
 
 
